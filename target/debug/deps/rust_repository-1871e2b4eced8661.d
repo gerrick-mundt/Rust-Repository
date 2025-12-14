531f5c805787db168a78cef6e97086ecd9b6aee7
gerrick mundt <gerrickmundt@gerricks-MacBook-Air.local>
2025-12-14 00:16:29 -0500

    Organize files into Project 1 folder

--- target/debug/deps/rust_repository-1871e2b4eced8661.d
+++
@@ -1,5 +0,0 @@
-/Users/gerrickmundt/Documents/rust-repository/target/debug/deps/rust_repository-1871e2b4eced8661.d: src/main.rs
-
-/Users/gerrickmundt/Documents/rust-repository/target/debug/deps/librust_repository-1871e2b4eced8661.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rust_repository-3258d321c1ac9bc4.d
+++
@@ -1,5 +0,0 @@
-/Users/gerrickmundt/Documents/rust-repository/target/debug/deps/rust_repository-3258d321c1ac9bc4.d: src/main.rs
-
-/Users/gerrickmundt/Documents/rust-repository/target/debug/deps/librust_repository-3258d321c1ac9bc4.rmeta: src/main.rs
-
-src/main.rs:
